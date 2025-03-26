302142867f31c74b6679fd75a165a5dfb1e4a5d0
jjlagunamoreno <jjlagunamoreno@gmail.com>
2025-03-26 10:39:29 +0100

    Add project files.

---
+++ MvcCoreApiCrudDepartamentos/Controllers/DepartamentosController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MvcCoreApiCrudDepartamentos.Controllers
+{
+    public class DepartamentosController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Cliente()
+        {
+            return View();
+        }
+
+    }
+}
